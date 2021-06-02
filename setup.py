2ec3556a142e04078525c066725752b50bbfe77a
Cherloy <48054620+Cherloy@users.noreply.github.com>
2021-06-03 01:03:50 +0300

    v1.1.1

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import setup, find_packages
 
-
 with open("ReadMe.md", 'r') as f:
     long_description = f.read()
 
 setup(
-    name='word_gena.py',
+    name='word_gener',
     version='1.0',
     author='Cherloy',
     author_email='sideswipe8@mail.ru',
     description='generate word file with random data',
-    long_description = long_description,
-    long_description_content_type = 'text/markdown',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/Cherloy/Word_gen',
-    packages = find_packages(),
+    packages=find_packages(),
     license='MIT',
     test_suite='tests',
     python_requires='>=3.6'
