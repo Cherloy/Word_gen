6fa70670fd34c2deea2fb59a3c1982b8ad6b5a4c
Cherloy <48054620+Cherloy@users.noreply.github.com>
2021-06-02 22:12:57 +0300

    v1.1

---
+++ tests/test_word.py
@@ -0,0 +1,9 @@
+import unittest
+from word_gen import random_string
+
+class TestWord_gen(unittest.TestCase):
+    def test_string_negative(self):
+        self.assertEqual(random_string(-2),0)
+
+    def test_string_pos(self):
+        self.assertEqual(random_string(4096), 4096)
